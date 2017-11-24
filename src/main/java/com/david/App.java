d09637e7d9c62e8700b866a2d37e53ecca4d334e
David Orelowitz <david.orelowitz@gmail.com>
2017-11-24 16:24:54 -0500

    First Commit

---
+++ src/main/java/com/david/App.java
@@ -0,0 +1,19 @@
+package com.david;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        String str = "Hello World!!";
+        StringServer server = new StringServer();
+
+        System.out.println(str);
+        System.out.println(server.toReverse(str));
+        System.out.println(server.toLower(str));
+        System.out.println(server.toUpper(str));
+        System.out.println(server.toRemoveSpaces(str));
+
+    }
+}
